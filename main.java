1f0671573d2e88f566bca0618394cdaf017aa0be
michael.J <79923488+michaelafk@users.noreply.github.com>
2022-06-24 16:29:05 +0200

    Update main.java

--- main.java
+++ main.java
@@ -4,26 +4,24 @@ public class main{
     s.inicio();
   }
   public void inicio(){
-    int [][] numeros = new int[]{{7,0,0},{0,0,0},{0,0,0}};
-    int dimensionx=numeros.lenght();
-    matriz m = new matriz(dimensionx,numeros[0].lenght());
-    m.setmatriz(numeros);
-    
-    if(!m.esvacia()){
-      if(!m.filascorrectas()){
-        if(!m.pivotecorrecto){
-          System.out.println(false);
+        int[][] numeros = new int[][]{{7, 0, 0}, {0, 0, 0}, {0, 1, 0}};
+        int dimensionx = numeros.length;
+        matriz m = new matriz(dimensionx, numeros[0].length);
+        m.setmatriz(numeros);
+
+        System.out.println(m);
+        if (!m.esVacia()) {
+            if (m.filascorrectas()) {
+                if (m.privotesCorrectos()) {
+                    System.out.println(true);
+                } else {
+                    System.out.println(false);
+                }
+            } else {
+                System.out.println(false);
+            }
+        } else {
+            System.out.println(true);
         }
-        else{
-          System.out.println(true);
-        }
-      }
-      else{
-        System.out.println(true);
-      }
-    }
-    else{
-      System.out.println(true);
-    }
   }
 }
